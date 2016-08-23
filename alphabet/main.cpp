1ba742c896d77ff175255d01b659b671ab9bcffe
pearson.lawrence <pearson.lawrenc@students.aie.edu.au>
2016-08-23 09:28:26 -0700

    introooo

--- alphabet/main.cpp
+++ alphabet/main.cpp
@@ -2,6 +2,24 @@
 
 int main()
 {
+	//list your char which is the character letter
+	int varA, varB, varC;
+	char charA, charB, charC;
+	// ask question
+	printf("Please list two letters of alphabet: ");
+	// %c is your char. You need two values for two numbers
+	scanf_s(" %c %c", &charA, 1, &charB, 1);
+	getchar();
+	int ascii1 = charA;
+	int ascii2 = charB;
+	// create third char and create two ascii values and 
+	//equal then to char A and B then create third askii and equal it to equation
 	
+	int askii3 = ((ascii1 + ascii2) / 2);
 
+	charC = askii3;
+
+	printf("The middle letter is: %c", charC);
+
+	getchar();
 }
